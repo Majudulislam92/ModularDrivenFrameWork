12c724c0937cf1193e525d0948fb0530f81c865a
Majudul Islam <Majudulislam92@gmail.com>
2017-05-28 13:03:44 -0400

    added funtions

--- src/utils/WDfuntions.java
+++ src/utils/WDfuntions.java
@@ -1,16 +1,35 @@
 package utils;
 
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
 public class WDfuntions {
 
-	public void typeByxpath (String locators, String values){
-		
+	public WebDriver driver;
+
+	public void typeByxpath (String locators, String values) {
+		driver.findElement(By.xpath(locators)).clear();
+		driver.findElement(By.xpath(locators)).sendKeys(values);
+
 	};
-		
+
 	public void typeByName(String locators, String values){
-	
+		driver.findElement(By.name(locators)).clear();
+		
+		driver.findElement(By.name(locators)).sendKeys(values);
+
 	};
 
 	public void typeById (String locators, String values){
-    };
-    	
+		
+		driver.findElement(By.id(locators)).clear();
+		driver.findElement(By.id(locators)).sendKeys(values);
+
+
+
+	};
+
 }
